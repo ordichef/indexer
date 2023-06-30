0e19cfada992e40f7995b60573a0413b231ce605
kiennd <kien.nguyenduc08@gmail.com>
2023-06-30 10:51:44 +0700

    wl

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import Brc20Transaction from "./brc20Transaction.route";
 import InscriptionRouter from "./inscription.route";
 
+let minted = 0;
+
 const initRoute = (app: express.Express) => {
   app.get("/", async (req: Request, res: Response) => {
     res.status(200).json({
@@ -16,6 +18,24 @@ const initRoute = (app: express.Express) => {
       },
     });
   });
+
+  app.get("/minted", async (req: Request, res: Response) => {
+    res.status(200).json({
+      data: {
+        minted: minted
+      },
+    });
+  });
+
+  app.post("/JDpHBCSxhRfD3yhOlL5u/minted", async (req: Request, res: Response) => {
+    minted = req.body.minted
+    res.status(200).json({
+      data: {
+        message: "success"
+      },
+    });
+  });
+
 
   app.use("/inscription", InscriptionRouter);
   app.use("/transaction", Brc20Transaction);
