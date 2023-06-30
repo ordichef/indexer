5f9ffeb35ea99ebbd0a9febd9abc82dbfb96cb31
kiennd <kien.nguyenduc08@gmail.com>
2023-06-30 10:55:24 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
 import config from './core/config';
-import { Inscription } from './models/inscription.model';
 import { initRoute } from './routes/route';
+import cors from 'cors';
 
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: ['https://onchainbean.com','http://localhost:*', 'http://localhost:5173', 'http://localhost:5174' ],
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
 initRoute(app);
 
 app.listen(config.PORT, () => {
